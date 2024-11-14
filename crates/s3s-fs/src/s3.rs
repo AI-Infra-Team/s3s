e2677bbdf71e1366a3858b3b2c0cd104bacfdad4
ActivePeter <1020401660@qq.com>
2024-11-14 15:44:43 +0800

    a

--- crates/s3s-fs/src/s3.rs
+++ crates/s3s-fs/src/s3.rs
@@ -363,9 +363,11 @@ impl S3 for FileSystem {
         let mut dir_queue: VecDeque<PathBuf> = default();
         dir_queue.push_back(path.clone());
 
+        let mut count = 0;
         while let Some(dir) = dir_queue.pop_front() {
             let mut iter = try_!(fs::read_dir(dir).await);
             while let Some(entry) = try_!(iter.next_entry().await) {
+                tracing::debug!("start processing dir: {:?} at {}", entry.path(), count);
                 let file_type = try_!(entry.file_type().await);
                 if file_type.is_dir() {
                     dir_queue.push_back(entry.path());
@@ -402,6 +404,8 @@ impl S3 for FileSystem {
                 }
             }
         }
+
+        tracing::debug!("list buckets finished");
 
         objects.sort_by(|lhs, rhs| {
             let lhs_key = lhs.key.as_deref().unwrap_or("");
